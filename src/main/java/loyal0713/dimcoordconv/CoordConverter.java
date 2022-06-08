d09450fe16a8afec39f61294e4dcd0bd9ba1404e
Josh Brown <42594335+Loyal0713@users.noreply.github.com>
2022-06-08 12:59:05 -0400

    Updated to 1.19
    - No longer using LiteralText, now uses Text interface
    - Updated gradle info
    - Upadate mod info

--- src/main/java/loyal0713/dimcoordconv/CoordConverter.java
+++ src/main/java/loyal0713/dimcoordconv/CoordConverter.java
@@ -5,7 +5,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
@@ -74,7 +74,7 @@ public class CoordConverter implements ModInitializer {
                 else message.append("north");
 
                 // tell player translated coords and direction
-                client.player.sendMessage(new LiteralText(message.toString()), false);
+                client.player.sendMessage(Text.of(message.toString()), false);
             }
         });
     }
